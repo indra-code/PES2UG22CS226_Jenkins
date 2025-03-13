9d391f413a4212f10d6aec6402848eefe41ece6c
indra-code <reachme.neel@gmail.com>
2025-03-13 16:02:25 +0530

    third commit

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World,I am PES2UG22CS226\n";
+  cout << "Hello, World,I am PES2UG22CS226,My name is INDRANEEL KVS\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
   return 0;
