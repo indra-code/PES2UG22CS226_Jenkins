0b2d5c4bbe24363d4bc75a113b3fed419b2185c1
indra-code <144906869+indra-code@users.noreply.github.com>
2025-03-13 16:30:24 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, World,I am PES2UG22CS226,My name is INDRANEEL KVS\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run this  \n"
+  cout << "I have successfully built and run this  \n";
   return 0;
 }
